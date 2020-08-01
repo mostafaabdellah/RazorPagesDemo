0ea9d092551ffa40f13f39a3d3aed1c205d2a99e
Mostaf Abdellah <mostafa.abdellah@hotmail.com>
2020-08-01 17:37:39 +0300

    Add project files.

---
+++ RazorPagesUI/Pages/Forms/AddAddress.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesUI.Models;
+
+namespace RazorPagesUI.Pages.Forms
+{
+    public class AddAddressModel : PageModel
+    {
+        [BindProperty]
+        public AddressModel Address { get; set; }
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid==false)
+            {
+                return Page();
+            }
+            return RedirectToPage("/index",new { City=Address.City});
+        }
+    }
+}
---
+++ RazorPagesUI/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace RazorPagesUI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty(SupportsGet  =true)]
+        public string City { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(City))
+                City = "The Web";
+        }
+    }
+}
